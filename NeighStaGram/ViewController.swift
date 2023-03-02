c5486cf9905f36bedee1122ddaaa85ea9446a54d
Abdallahi Thiaw <abcodez21@outlook.com>
2023-03-02 10:01:02 -0500

    Initial Commit

---
+++ NeighStaGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeighStaGram
+//
+//  Created by Abdallahi Thiaw on 3/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
